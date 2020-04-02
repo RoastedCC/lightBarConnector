07b0457facf340c196395668ef668186820bd0e6
GraayBird <48427287+RoastedCC@users.noreply.github.com>
2020-04-02 10:08:47 -0800

    Test

--- src/main/java/com/lightBar/LightBarPlugin.java
+++ src/main/java/com/lightBar/LightBarPlugin.java
@@ -3,10 +3,9 @@ package com.lightBar;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -21,10 +20,17 @@ import java.io.IOException;
 )
 public class LightBarPlugin extends Plugin
 {
+
+	@Inject LightBarConfig config;
+	public SerialPort serialPort = new SerialPort(config.COMP());
+	public String comboString;
+	public Actor myActor;
+	public Widget widget;
+	private String isPoison;
+	private String isVenomed;
+	public String prevString;
 	@Inject
 	private Client client;
-	@Inject LightBarConfig config;
-	private SerialPort serialPort = new SerialPort(config.COMP());
 	@Inject
 	@Override
 	protected void startUp() throws Exception
@@ -68,6 +74,7 @@ public class LightBarPlugin extends Plugin
 
 	}
 
+
 	@Override
 	protected void shutDown() throws Exception
 	{
@@ -90,6 +97,42 @@ public class LightBarPlugin extends Plugin
 	LightBarConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(LightBarConfig.class);
+	}
+
+
+	public void data(){
+		comboString = myActor.getHealthRatio() + isPoison + isVenomed;
+		if(comboString != prevString){send();}
+	}
+
+	public void checkPoison() {
+		if (widget.createChild(160, 6).getSpriteId() == SpriteID.MINIMAP_ORB_HITPOINTS_POISON) {
+			isPoison = "+";
+		} else if (widget.createChild(160, 6).getSpriteId() == SpriteID.MINIMAP_ORB_HITPOINTS_VENOM) {
+			isVenomed = "+";
+		} else {
+			isPoison = "-";
+			isVenomed = "+";
+		}
+	}
+
+	public void send(){
+		prevString = comboString;
+		try {
+			serialPort.openPort();
+			serialPort.setParams(SerialPort.BAUDRATE_9600,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+			serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN |
+					SerialPort.FLOWCONTROL_RTSCTS_OUT);
+			serialPort.writeString(comboString);
+			data();
+		}
+		catch (SerialPortException ex) {
+			System.out.println("There are an error on writing string to port т: " + ex);
+			data();
+		}
 	}
 
 }
